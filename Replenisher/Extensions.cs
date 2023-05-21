86cffc0153c15458f261f9a3d3b2d841d6c98bc8
Sooofa <emr3.cbc@gmail.com>
2023-05-22 01:29:14 +0300

    Updated to API 2.1 and some code clean up

---
+++ Replenisher/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace Replenisher {
+    public static class Extensions {
+        public static string FirstCharToUpper(this string input) {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentException("String cannot be empty.");
+            return input.First().ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
